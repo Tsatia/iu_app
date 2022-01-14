f19ef8636a5ca02dd9a47bc2a12d3727020b44db
Tsatia <fbcedric@yahoo.fr>
2022-01-14 22:03:00 +0100

    Update init db

--- db/init.sql
+++ db/init.sql
@@ -2,8 +2,8 @@ CREATE DATABASE iu_test_db;
 use iu_test_db;
 
 CREATE TABLE iu_test (
-  name VARCHAR(20),
-  description VARCHAR(10)
+  name VARCHAR(100),
+  description VARCHAR(100)
 );
 
 INSERT INTO iu_test
